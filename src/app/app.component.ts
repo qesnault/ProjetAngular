8635a82a34de1f8c6051aaa235e587be1e98ac3d
qesnault <quentin.esnault@lilo.org>
2020-11-25 15:13:16 +0100

    add genius api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GetApiService } from  './get-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet-angular';
+  constructor(private api:GetApiService)
+  {
+
+  }
+  ngOnInit()
+  {
+    this.api.apiCall().subscribe((data =>{
+      console.log("get api data", data);
+    }));
+  }
 }
---
+++ src/app/get-api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetApiService {
+
+  header: Object = {
+    "headers": {
+      "x-rapidapi-host": "genius.p.rapidapi.com",
+      "x-rapidapi-key": "a7614406e2msh59ee1345507dfb4p186de0jsn2bf93e973b3a"
+    }
+  }
+
+  fetchedArtirts: Object[] = []
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  apiCall()
+  {
+    return this.http.get('https://genius.p.rapidapi.com/artists/16775', this.header);
+  }
+}
